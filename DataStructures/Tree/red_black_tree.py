f2ce85eff8acdc4c60d9fff5f84831a8cd30ddbd
21salazarf <fa.salazar@uniandes.edu.co>
2025-10-24 15:51:55 -0500

    b

--- DataStructures/Tree/red_black_tree.py
+++ DataStructures/Tree/red_black_tree.py
@@ -1,9 +1,10 @@
-from Tree import rbt_node as rbn
+from DataStructures.Tree import rbt_node as rbn
 
 def new_map():
     my_map = {}
     my_map["root"] = None
     my_map["size"] = 0
     return my_map
+
 
 
