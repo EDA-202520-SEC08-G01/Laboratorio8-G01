fd045d3610cac6e60646f74559da269a65c6ecaf
Katxirolii <je.espitia@uniandes.edu.co>
2025-10-29 20:29:22 -0500

    hola commit

--- DataStructures/Tree/red_black_tree.py
+++ DataStructures/Tree/red_black_tree.py
@@ -6,26 +6,101 @@ def new_map():
     my_map["type"] = "RBT"
     return my_map
 
-def default_compare(key, element):
-    if key == rbn.get_key(element):    
-        return 0
-    elif key > rbn.get_key(element):
+def default_compare(key1, key2):
+    if key1 < key2:
+        return -1
+    elif key1 > key2:
         return 1
     else:
-        return -1
+        return 0
     
-def rotate_left(node_rbt):
-    node1 = node_rbt["right"]
-    l_node1 = node1["left"]
-    l_rbt   = node_rbt["left"]
-    node_rbt["left"] = node_rbt
-    node_rbt = node1
-    node_rbt["left"]["right"] = l_node1
-    node_rbt["left"]["left"] = l_rbt
+def rotate_left(node):
+    if node is None or node["right"] is None:
+        return node
+    
+    new_root = node["right"]
+    node["right"] = new_root["left"]
+    new_root["left"] = node
+    return new_root
 
-    rbn.change_color(node_rbt["left"], 0)
-    rbn.change_color(node_rbt["right"], 0)
-    rbn.change_color(node_rbt["left"], 1)
+def rotate_right(node):
+    if node is None or node["left"] is None:
+        return node
+    
+    new_root = node["left"]
+    node["left"] = new_root["right"]
+    new_root["right"] = node
+    return new_root
 
-    return node_rbt
+def flip_colors(node):
+    if node is None:
+        return
+    node["color"] = rbn.RED
+    if node["left"]:
+        node["left"]["color"] = rbn.BLACK
+    if node["right"]:
+        node["right"]["color"] = rbn.BLACK
+        
+def flip_node_color(node):
+    if node is None:
+        return
+    if node["color"] == rbn.RED:
+        node["color"] = rbn.BLACK
+    else:
+        node["color"] = rbn.RED
+        
+def is_red(node):
+    if node is None:
+        return False
+    return node["color"] == rbn.RED
 
+def size_tree(root):
+    if root is None:
+        return 0
+    return root["size"]
+
+def insert_node(root, key, value):
+    if root is None:
+        return rbn.new_rbt_node(key, value, rbn.RED)
+    
+    cmp = default_compare(key, root["key"])
+    if cmp < 0:
+        root["left"] = insert_node(root["left"], key, value)
+    elif cmp > 0:
+        root["right"] = insert_node(root["right"], key, value)
+    else:
+        root["value"] = value
+
+    if is_red(root["right"]) and not is_red(root["left"]):
+        root = rotate_left(root)
+    if is_red(root["left"]) and is_red(root["left"]["left"]):
+        root = rotate_right(root)
+    if is_red(root["left"]) and is_red(root["right"]):
+        flip_colors(root)
+
+    root["size"] = 1 + size_tree(root["left"]) + size_tree(root["right"])
+    return root
+
+def put(my_rbt, key, value):
+    my_rbt["root"] = insert_node(my_rbt["root"], key, value)
+    my_rbt["root"]["color"] = rbn.BLACK
+    
+    return my_rbt
+
+def get(my_rbt, key):
+    return get_node(my_rbt["root"], key)
+
+def get_node(node, key):
+    k = rbn.get_key(node)
+    if node is None:
+        return None
+    elif k > key:
+        return get_node(node["right"], key)
+    elif k < key:
+        return get_node(node["left"], key)
+    elif k == key:
+        return rbn.get_value(node)
+    else:
+        return None
+
+    
